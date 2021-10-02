07ab1dcf41c3da8f4ef0c8f3f4fbd034ff0c9ea3
keshav0114 <69249141+keshav0114@users.noreply.github.com>
2021-10-02 17:00:02 +0530

    Update Login.jsx
    Updated

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,31 +1,30 @@
 import React from "react";
 import './Login.css';
+import {NavLink} from "react-router-dom"
 
-const Login = ()  =>{
-
- 
+const Login = ()  =>{ 
     return(
       <>
-      <div className="main_div">
           <div className="login_side">
-              <img className="login_img" src="img/heading.svg" alt="Graphic"/>
+              <img className="login_img" src="assets/heading.svg" alt="Graphic"/>
               <div className="form_area">
-              <div className="registerFields">
-                    <img className="user_svg" src="img/user.svg" />
-                    <input type="text" className="input1" placeholder="Roll Number"></input>
-               </div>
-               <div className="registerFields">
-                    <img className="pass_svg" src="img/pass.svg"/>
-                    <input type="password" className="input2" placeholder="Password"></input>
-               </div>
-               <button className="submit_button">Login</button>
-               <p className="lastLine"> Not registered yet? <span>Register</span></p>
+              <form>
+                  <div className="registerFields">
+                     <img className="user_svg" src="assets/user.svg" />
+                     <input type="text" className="input1" placeholder="Roll Number"/>
+                  </div>
+                  <div className="registerFields">
+                     <img className="pass_svg" src="assets/pass.svg"/>
+                     <input type="password" className="input2" placeholder="Password"/>
+                  </div>
+              </form>
+               <button className="submit_button"><NavLink exact activeclassName="ActiveClass" to="/instruction">Login</NavLink></button>
+               <p className="lastLine"> Not registered yet? <span><NavLink exact activeclassName="ActiveClass" to="/register">Register</NavLink></span></p>
                </div>
           </div>
           <div className="img_side">
-              <img className="girl_img" src="img/illustration.png" alt="Graphic"/>
+              {/* <img className="girl_img" src="assets/illustration.png" alt="Graphic"/> */}
           </div>
-      </div>
       </>
   );
 };
