6cf0ede7e21dd3a8359f973ae3b82288dba4b3fd
Divykant <divykant1921107@akgec.ac.in>
2021-10-05 22:48:40 +0530

    Login Complete

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -6,23 +6,28 @@ const Login = ()  =>{
  
     return(
       <>
-      <div className="main_div">
-          <div className="login_side">
+      <div className="maindiv">
+          <div className="login_Leftside">
               <img className="login_img" src="img/heading.svg" alt="Graphic"/>
               <div className="form_area">
-              <div className="registerFields">
+                <div className="registerFields">
                     <img className="user_svg" src="img/user.svg" />
                     <input type="text" className="input1" placeholder="Roll Number"></input>
-               </div>
-               <div className="registerFields">
+                 </div>
+                <div className="registerFields">
                     <img className="pass_svg" src="img/pass.svg"/>
                     <input type="password" className="input2" placeholder="Password"></input>
-               </div>
-               <button className="submit_button">Login</button>
-               <p className="lastLine"> Not registered yet? <span>Register</span></p>
+                </div>
+                <div className="lastdiv">
+                    <button className="submit_button">Login</button>
+                </div>
+                <div className="lastdiv">
+                <p className="lastLine"> Not registered yet? <a href="#" >Register</a></p>
+                
+                </div>
                </div>
           </div>
-          <div className="img_side">
+          <div className="img_Righside">
               <img className="girl_img" src="img/illustration.png" alt="Graphic"/>
           </div>
       </div>
